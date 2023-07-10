5bd0cafc91e67be45e6da2d11ab285a0928c07a8
thanhtran <quocthanh2694@gmail.com>
2023-07-10 16:36:56 +0700

    chore: Add web version

--- packages/container/src/App.tsx
+++ packages/container/src/App.tsx
@@ -3,6 +3,8 @@ import Router from "./Router";
 import "./App.scss";
 import "./styles/global.scss";
 
+console.log("@@Container v1.0.0");
+
 const App = () => {
   return <Router />;
 };
--- packages/shop/src/App.tsx
+++ packages/shop/src/App.tsx
@@ -6,6 +6,8 @@ import "./styles/global.scss";
 import { ModalContextType, ModalKey } from "./Modals/type";
 import { AppProvider } from "./context/provider";
 
+console.log('@@Shop v1.0.0');
+
 const App = () => {
   const [modalData, setModalData] = useState<ModalContextType>(
     modalContextInitValue
