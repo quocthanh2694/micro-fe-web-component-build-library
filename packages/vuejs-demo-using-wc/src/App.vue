f0dbcfb35929423a7084f74e56c2c9d475740fc7
thanhtran <quocthanh2694@gmail.com>
2023-07-10 16:36:20 +0700

    chore: Init Vue project for testing web component input

---
+++ packages/vuejs-demo-using-wc/src/App.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import '../../ui-libs/src/index';
+</script>
+
+<template>
+  <div>
+    <h2>Vue Demo Web Component Input</h2>
+    <div>
+      <form @submit.prevent="submitForm">
+        <!-- <input v-model="formValue" type="text" placeholder="Enter your name" required /> -->
+        <pj1-input v-model="formValue" @onchange="onChange" type="text" placeholder="Place holder for input" required />
+        <br>
+        <input type="submit" value="Submit" />
+      </form>
+      <br>
+      <code>Form value: {{ formValue }}</code>
+    </div>
+  </div>
+</template>
+
+<script  lang="ts">
+export default {
+  data() {
+    return {
+      formValue: "",
+      formSubmitted: false,
+    };
+  },
+  methods: {
+    submitForm: function () {
+      this.formSubmitted = true;
+      alert(this.formValue);
+    },
+    onChange: (e: any) => {
+      console.log('@@onChange', e?.detail?.target?.value)
+    }
+  },
+};
+</script>
+
+<style scoped></style>
